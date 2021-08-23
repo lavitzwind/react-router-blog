f7122e7612f0fc36e6c224f2701655d421bf647f
milleseseau <leojimlavi@gmail.com>
2021-08-23 06:07:15 -0500

    cleaning, refactoring component states

--- src/EditPost.js
+++ src/EditPost.js
@@ -1,10 +1,14 @@
-import { useEffect, useContext } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useState, useEffect, useContext } from "react";
+import { useParams, Link, useHistory } from "react-router-dom";
+import { format } from "date-fns";
+import api from "./api/posts";
 import DataContext from "./context/DataContext";
 
-const EditPost = ({}) => {
-	const { posts, handleEdit, editBody, setEditBody, editTitle, setEditTitle } =
-		useContext(DataContext);
+const EditPost = () => {
+	const [editTitle, setEditTitle] = useState("");
+	const [editBody, setEditBody] = useState("");
+	const { posts, setPosts } = useContext(DataContext);
+	const history = useHistory();
 	const { id } = useParams();
 	const post = posts.find((post) => post.id.toString() === id);
 
@@ -14,6 +18,20 @@ const EditPost = ({}) => {
 			setEditBody(post.body);
 		}
 	}, [post, setEditTitle, setEditBody]);
+
+	const handleEdit = async (id) => {
+		const datetime = format(new Date(), "MMMM dd, yyyy pp");
+		const updatedPost = { id, title: editTitle, datetime, body: editBody };
+		try {
+			const response = await api.put(`/posts/${id}`, updatedPost);
+			setPosts(posts.map((post) => (post.id ? { ...response.data } : post)));
+			setEditTitle("");
+			setEditBody("");
+			history.push("/");
+		} catch (err) {
+			console.log(`Error: ${err.message}`);
+		}
+	};
 
 	return (
 		<main className="NewPost">
--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -1,8 +1,5 @@
 import { createContext, useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
-import { format } from "date-fns";
-import api from "../api/posts";
-import useWindowSize from "../hooks/useWindowSize";
 import useAxiosFetch from "../hooks/useAxiosFetch";
 
 const DataContext = createContext({});
@@ -11,12 +8,6 @@ export const DataProvider = ({ children }) => {
 	const [posts, setPosts] = useState([]);
 	const [search, setSearch] = useState("");
 	const [searchResults, setSearchResults] = useState([]);
-	const [postTitle, setPostTitle] = useState("");
-	const [postBody, setPostBody] = useState("");
-	const [editTitle, setEditTitle] = useState("");
-	const [editBody, setEditBody] = useState("");
-	const history = useHistory();
-	const { width } = useWindowSize();
 
 	const { data, fetchError, isLoading } = useAxiosFetch(
 		"http://localhost:3500/posts"
@@ -35,69 +26,16 @@ export const DataProvider = ({ children }) => {
 		setSearchResults(filteredResults.reverse());
 	}, [posts, search]);
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
-		const datetime = format(new Date(), "MMMM dd, yyyy pp");
-		const newPost = { id, title: postTitle, datetime, body: postBody };
-		try {
-			const response = await api.post("/posts", newPost);
-			const allPosts = [...posts, response.data];
-			setPosts(allPosts);
-			setPostTitle("");
-			setPostBody("");
-			history.push("/");
-		} catch (err) {
-			console.log("Error: ${err.message}");
-		}
-	};
-
-	const handleDelete = async (id) => {
-		try {
-			await api.delete(`/posts/${id}`);
-			const postsList = posts.filter((post) => post.id !== id);
-			setPosts(postsList);
-			history.push("/");
-		} catch (err) {
-			console.log(`Error: ${err.message}`);
-		}
-	};
-
-	const handleEdit = async (id) => {
-		const datetime = format(new Date(), "MMMM dd, yyyy pp");
-		const updatedPost = { id, title: editTitle, datetime, body: editBody };
-		try {
-			const response = await api.put(`/posts/${id}`, updatedPost);
-			setPosts(posts.map((post) => (post.id ? { ...response.data } : post)));
-			setEditTitle("");
-			setEditBody("");
-			history.push("/");
-		} catch (err) {
-			console.log(`Error: ${err.message}`);
-		}
-	};
-
 	return (
 		<DataContext.Provider
 			value={{
-				width,
 				search,
 				setSearch,
 				searchResults,
 				fetchError,
 				isLoading,
-				handleSubmit,
-				postTitle,
-				setPostTitle,
-				postBody,
-				setPostBody,
 				posts,
-				handleEdit,
-				editBody,
-				setEditBody,
-				editTitle,
-				setEditTitle,
-				handleDelete,
+				setPosts,
 			}}
 		>
 			{children}
