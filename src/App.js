01870b71dc1a2bdd6c606735ad889bbacbeeed1d
milleseseau <leojimlavi@gmail.com>
2021-08-30 04:59:08 -0500

    refactoring again

--- src/App.js
+++ src/App.js
@@ -8,23 +8,20 @@ import EditPost from "./EditPost";
 import About from "./About";
 import Missing from "./Missing";
 import { Route, Switch } from "react-router-dom";
-import { DataProvider } from "./context/DataContext";
 
 function App() {
 	return (
 		<div className="App">
 			<Header title="React JS Blog" />
-			<DataProvider>
-				<Nav />
-				<Switch>
-					<Route exact path="/" component={Home} />
-					<Route exact path="/post" component={NewPost} />
-					<Route path="/edit/:id" component={EditPost} />
-					<Route path="/post/:id" component={PostPage} />
-					<Route path="/about" component={About} />
-					<Route path="*" component={Missing} />
-				</Switch>
-			</DataProvider>
+			<Nav />
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route exact path="/post" component={NewPost} />
+				<Route path="/edit/:id" component={EditPost} />
+				<Route path="/post/:id" component={PostPage} />
+				<Route path="/about" component={About} />
+				<Route path="*" component={Missing} />
+			</Switch>
 			<Footer />
 		</div>
 	);
