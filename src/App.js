103c3ab178faff6300d8727a0b61b2a69c2e59a3
milleseseau <leojimlavi@gmail.com>
2021-08-30 05:35:49 -0500

    more refactoring

--- src/App.js
+++ src/App.js
@@ -8,14 +8,29 @@ import EditPost from "./EditPost";
 import About from "./About";
 import Missing from "./Missing";
 import { Route, Switch } from "react-router-dom";
+import { useEffect } from "react";
+import useAxiosFetch from "./hooks/useAxiosFetch";
+import { useStoreActions } from "easy-peasy";
 
 function App() {
+	const setPosts = useStoreActions((actions) => actions.setPosts);
+
+	const { data, fetchError, isLoading } = useAxiosFetch(
+		"http://localhost:3500/posts"
+	);
+
+	useEffect(() => {
+		setPosts(data);
+	}, [data, setPosts]);
+
 	return (
 		<div className="App">
 			<Header title="React JS Blog" />
 			<Nav />
 			<Switch>
-				<Route exact path="/" component={Home} />
+				<Route exact path="/">
+					<Home isLoading={isLoading} fetchError={fetchError} />
+				</Route>
 				<Route exact path="/post" component={NewPost} />
 				<Route path="/edit/:id" component={EditPost} />
 				<Route path="/post/:id" component={PostPage} />
